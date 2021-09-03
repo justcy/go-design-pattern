e68a89651ca98aa720b4f222704591cc477b3039
chenyu <1007577820@qq.com>
2021-09-03 10:00:06 +0800

    增加桥接模式

--- bridge/bridge.go
+++ bridge/bridge.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type Abstraction interface {
 	Operation()
 
@@ -8,12 +10,58 @@ type RefinedAbstraction interface {
 	Abstraction
 }
 type Implementor interface {
-	RefinedAbstraction
+	OperationImpl()
 }
 
 type ConcreteImplementorA struct {
 
 }
+
+func NewImplementorA() Implementor  {
+	return &ConcreteImplementorA{}
+}
+func (c *ConcreteImplementorA) OperationImpl() {
+	fmt.Println("This is Implementor A")
+}
+
 type ConcreteImplementorB struct {
 
+}
+
+func NewImplementorB() Implementor  {
+	return &ConcreteImplementorB{}
+}
+
+func (c *ConcreteImplementorB) OperationImpl() {
+	fmt.Println("This is Implementor B")
+}
+
+type CommonImplementor struct {
+	method Implementor
+}
+
+func (c *CommonImplementor) Operation() {
+	fmt.Println("CommonImplementor1 Call")
+	c.method.OperationImpl()
+}
+
+func NewCommonImplementor(method Implementor)  *CommonImplementor {
+	return &CommonImplementor{
+		method:method,
+	}
+}
+
+type CommonImplementor2 struct {
+	method Implementor
+}
+
+func (c *CommonImplementor2) Operation() {
+	fmt.Println("CommonImplementor2 Call")
+	c.method.OperationImpl()
+}
+
+func NewCommonImplementor2(method Implementor)  *CommonImplementor2 {
+	return &CommonImplementor2{
+		method:method,
+	}
 }
--- bridge/main.go
+++ bridge/main.go
@@ -1,5 +1,15 @@
 package main
 
 func main() {
+	m := NewCommonImplementor(NewImplementorA())
+	m.Operation()
 
+	m = NewCommonImplementor(NewImplementorB())
+	m.Operation()
+
+	m2 := NewCommonImplementor2(NewImplementorA())
+	m2.Operation()
+
+	m2 = NewCommonImplementor2(NewImplementorB())
+	m2.Operation()
 }
